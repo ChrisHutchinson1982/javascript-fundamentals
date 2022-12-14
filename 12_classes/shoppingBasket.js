1a5c13e69a50bbea18e3ce8c2200b4fca23eb72f
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-12-14 14:11:02 +0000

    Project setup

--- 12_classes/shoppingBasket.js
+++ 12_classes/shoppingBasket.js
@@ -23,18 +23,3 @@ class ShoppingBasket {
 }
 
 module.exports = ShoppingBasket;
-
-// > const basket = new ShoppingBasket();
-// > basket.getTotalPrice();
-// 0
-
-// > basket.addItem(candy);
-
-// > basket.getTotalPrice();
-// 4.99
-
-// > basket.addItem(new Candy('Skittle', 3.99));
-// > basket.addItem(new Candy('Skittle', 3.99));
-
-// > basket.getTotalPrice();
-// 12.97
